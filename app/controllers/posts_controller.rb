e349eaf0ee40499ab87c5a457d1c82afd23a2b68
kroxy <kreiter.tibor@gmail.com>
2015-03-17 11:53:36 +0100

    Install will_paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except:[:index,:show]
   before_action :find_post, only: [:show,:edit,:update,:destroy]
   def index
-    @posts=Post.all.order("created_at DESC")
+    @posts=Post.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
   def new
