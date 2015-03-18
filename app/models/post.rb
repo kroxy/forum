436ba1040e9fa6bb86657dae7f6b436ba6fb725a
kroxy <kreiter.tibor@gmail.com>
2015-03-18 12:58:48 +0100

    design fine resizing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  validates :title, presence: true
+  validates :title, presence: true,length: { maximum: 150 }
   validates :content, presence: true,length: { minimum: 50 }
   has_attached_file :image, styles: { :medium => "300x300>", }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
