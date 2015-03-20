44b8e15040ecd4b995c979c140247f09463abf8f
kroxy <kreiter.tibor@gmail.com>
2015-03-21 00:36:32 +0100

    facebook autenticate with email

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
   def apply_omniauth(auth)
     # In previous omniauth, 'user_info' was used in place of 'raw_info'
-    self.email = "ez@email.hu"
+    @email = auth['extra']['raw_info']['email']
     # Again, saving token is optional. If you haven't created the column in authentications table, this will fail
     authentications.build(:provider => auth['provider'], :uid => auth['uid'], :token => auth['credentials']['token'])
   end
